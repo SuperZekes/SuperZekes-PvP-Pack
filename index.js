1bd6caf48f8acdb00503ca96f72391b81b318036
SuperZekes <149198602+SuperZekes@users.noreply.github.com>
2023-12-02 21:43:44 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Hi this is my pvp pack!</h2>
+
+<p id="demo">I hope you test it out!</p>
+
+<button type="button" onclick='document.getElementById("demo").innerHTML = "Hello JavaScript!"'>Click Me!</button>
+
+</body>
+</html>
