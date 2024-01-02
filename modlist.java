8c5ffd0734bdb2b17bbe08cd1722812d052369ba
SuperZekes <149198602+SuperZekes@users.noreply.github.com>
2024-01-02 12:17:12 -0400

    Create modlist.java

---
+++ modlist.java
@@ -0,0 +1,37 @@
+BetterF3
+Debugify
+ImmediatelyFast
+No Chat Reports
+Resourcify
+Xaero's World Map
+Xaero's Minimap
+Zoomify
+AppleSkin
+BetterHurtCam
+Better Statistics Screen
+Chat Heads
+Cloth Config API
+Dynamic FPS
+Fabric API
+Fabric Language Kotlin
+FerriteCore
+Indium
+Iris Shaders
+Lithium
+Mod Menu
+Reese's Sodium Options
+ReplayMod
+Show Me Your Skin!
+3D Skin Layers
+Totem Small pop
+Sodium
+TotemCounter
+ukulib
+ViaFabricPlus
+Very Many Players (Fabric)
+Wavey Capes
+World Host
+YetAnotherConfigLib
+Crosshair TXF
+Default Dark Mode
+Fast Better Grass
